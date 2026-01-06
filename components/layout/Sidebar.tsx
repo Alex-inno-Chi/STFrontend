e9aafec9a5a362bbfef54b7bb6699e9fbb173fc3
yuliyakarastsialiova-lang <yuliya.karastsialiova@innowise.com>
2026-01-06 13:13:09 +0300

    Revert "feat: token auth without redirect"

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,10 +1,8 @@
 "use client";
 import { ExitIcon, FaceIcon, Cross2Icon } from "@radix-ui/react-icons";
-import { usePathname, redirect } from "next/navigation";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { useAdminStore } from "@/lib/store/admin";
-import { deleteSession } from "@/lib/api/jwt_session";
-import { toast } from "react-toastify";
 
 export default function Sidebar() {
   const { setSidebarIsOpen, sidebarIsOpen } = useAdminStore();
@@ -16,12 +14,6 @@ export default function Sidebar() {
   }
 
   const navItems = [{ name: "Chats", icon: FaceIcon, path: "/" }];
-
-  const logOut = async () => {
-    await deleteSession();
-    toast.success(`User loged out !`);
-    redirect("/login");
-  };
 
   return (
     <>
@@ -72,10 +64,7 @@ export default function Sidebar() {
                 </li>
               );
             })}
-            <li
-              className="flex items-center p-2 hover:bg-gray-200 rounded cursor-pointer text-red-600"
-              onClick={() => logOut()}
-            >
+            <li className="flex items-center p-2 hover:bg-gray-200 rounded cursor-pointer text-red-600">
               <ExitIcon className="w-5 h-5 mr-2" />
               <span>Logout</span>
             </li>
--- lib/api/auth.ts
+++ lib/api/auth.ts
@@ -3,7 +3,6 @@ import { toast } from "react-toastify";
 import { POST } from "./client";
 import { ApiEndpoints } from "./api-endpoints";
 import { User } from "../types";
-import { createSession } from "./jwt_session";
 
 export interface RegisterUserData {
   username: string;
@@ -24,7 +23,6 @@ export const loginUserAPI = async (
 
     if (response.ok) {
       toast.success(`User loged in !`);
-      await createSession(response.data.user.id);
       return response.data.user;
     }
     return null;
@@ -42,7 +40,6 @@ export const registerUserAPI = async (
 
     if (response.ok) {
       toast.success(`User created !`);
-      await createSession(response.data.user.id);
       return response.data.user;
     }
     return null;
