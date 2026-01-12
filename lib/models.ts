aee5e551a6d95b942b0bb84d590df59e5774804e
Yuliya Karastsialiova <yuliya.karastsialiova@innowise.com>
2026-01-12 17:42:12 +0300

    feat: added creating private and group chats

--- lib/models.ts
+++ lib/models.ts
@@ -8,3 +8,12 @@ export const loginSchema = z.object({
 export const registerSchema = loginSchema.extend({
   username: z.string().min(3, "Username must be at least 3 characters"),
 });
+
+export const privateChatSchema = z.object({
+  otherUserId: z.coerce.number({ invalid_type_error: "Select member" }),
+});
+
+export const groupChatSchema = z.object({
+  name: z.string().trim().min(1, "Chat name must be at least 1 character"),
+  memberIds: z.array(z.coerce.number()).min(1, "Select at least one member"),
+});
