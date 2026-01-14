0a3f57ecd42867d8f5910391c7fe0ea14085be76
Yuliya Karastsialiova <yuliya.karastsialiova@innowise.com>
2026-01-14 20:13:53 +0300

    feat: message bubbles

--- components/ui/ChatWindow.tsx
+++ components/ui/ChatWindow.tsx
@@ -1,4 +1,5 @@
 import { Message } from "@/lib/types";
+import MessageList from "./MessageList";
 
 interface ChatWindowProps {
   userId: number | null;
@@ -8,16 +9,24 @@ interface ChatWindowProps {
   handleDeleteMessage: (id: number | null) => void;
 }
 
-export default function ChatWindow({ chatId }: ChatWindowProps) {
+export default function ChatWindow({
+  userId,
+  chatId,
+  messages,
+}: ChatWindowProps) {
   return (
     <div
-      className={`sm:flex ${chatId ? "flex" : "hidden"} flex-1 flex-col h-full max-h-screen relative`}
+      className={`sm:flex ${chatId ? "flex" : "hidden"} flex-1 flex-col h-full max-h-screen relative p-[10px]`}
     >
-      {!chatId && (
+      {!chatId ? (
         <div className="flex items-center justify-center w-full h-full">
           <h2 className="text-lg font-semibold">
             Select a chat to start messaging
           </h2>
+        </div>
+      ) : (
+        <div className="flex-1 overflow-y-auto">
+          <MessageList messages={messages} userId={userId} />
         </div>
       )}
     </div>
