51b7340ebb638ff8c24bb113d51ba79424eff057
hapurzhonau-inno <daniyar.hapurzhonau@innowise.com>
2026-01-15 12:13:48 +0300

    redirect implementation

--- providers/AuthGuard.tsx
+++ providers/AuthGuard.tsx
@@ -1,29 +1,25 @@
 "use client";
 
+import { useEffect } from "react";
+import { useRouter, usePathname } from "next/navigation";
 import { useUserStore } from "@/lib/store/user";
-import { useRouter } from "next/navigation";
-import { useCallback, useEffect } from "react";
 
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
-  const { user, isInitialized } = useUserStore((state) => ({
-    user: state.user,
-    isInitialized: state.isInitialized,
-  }));
+  const { user } = useUserStore();
   const router = useRouter();
-  const checkAuth = useCallback(() => {
-    if (!isInitialized) return;
-    if (user === null) router.push("/login");
-  }, [user, isInitialized, router]);
+  const pathname = usePathname();
+
   useEffect(() => {
-    checkAuth();
-  }, [checkAuth]);
-  //toDo spinner
-  if (!isInitialized) {
-    return (
-      <div className="flex h-screen items-center justify-center">
-        <div className="text-gray-600">Loading…</div>
-      </div>
-    );
-  }
+    if (!user?.id) {
+      router.push("/login");
+      return;
+    }
+
+    if (user && pathname === "/login") {
+      router.replace("/");
+      return;
+    }
+  }, [user, router, pathname]);
+
   return <>{children}</>;
 }
