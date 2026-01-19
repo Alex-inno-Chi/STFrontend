25612272e51f040a5aeb804d3a2924af47b38421
Yuliya Karastsialiova <yuliya.karastsialiova@innowise.com>
2026-01-19 19:58:16 +0300

    fix: checking messages

--- components/ui/ChatWindow.tsx
+++ components/ui/ChatWindow.tsx
@@ -16,18 +16,25 @@ export default function ChatWindow({
 }: ChatWindowProps) {
   return (
     <div
-      className={`sm:flex ${chatId ? "flex" : "hidden"} flex-1 flex-col h-full max-h-screen relative p-[10px]`}
+      className={`sm:flex ${chatId ? "flex" : "hidden"} flex-1 flex-col h-full max-h-screen relative `}
     >
-      {!chatId ? (
+      {!chatId && (
         <div className="flex items-center justify-center w-full h-full">
           <h2 className="text-lg font-semibold">
             Select a chat to start messaging
           </h2>
         </div>
-      ) : (
-        <div className="flex-1 overflow-y-auto">
+      )}
+      {messages.length > 0 ? (
+        <div className="flex-1 overflow-y-auto p-[10px]">
           <MessageList messages={messages} userId={userId} />
         </div>
+      ) : (
+        chatId && (
+          <div className="flex items-center justify-center w-full h-full">
+            <h2 className="text-lg font-semibold">No messages yet</h2>
+          </div>
+        )
       )}
     </div>
   );
