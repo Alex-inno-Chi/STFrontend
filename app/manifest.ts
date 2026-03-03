aa85341504e8595edf543272006d0b9469487d59
Ksana Ivanova <ksenia.rad.00@mail.ru>
2026-03-04 01:20:45 +0300

    update PWA icons and favicon

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,7 +2,7 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "ST-v0.1",
+    name: "ST-v0.2",
     short_name: "SimpleT",
     description: "A Progressive Web App built with Next.js",
     start_url: "/",
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     icons: [
       {
-        src: "/dogs.svg",
+        src: "/app-icon-192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/vercel.svg",
+        src: "/app-icon-512.png",
         sizes: "512x512",
         type: "image/png",
       },
