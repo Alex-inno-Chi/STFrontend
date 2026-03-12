d0c4ea104bc9acf7a58e925dc2f5d9f5abae5d29
Ksana Ivanova <ksenia.rad.00@mail.ru>
2026-03-12 09:27:27 +0300

    refactor: change chat window for future message bubble

--- components/ui/ChatWindow.tsx
+++ components/ui/ChatWindow.tsx
@@ -1,4 +1,5 @@
 import { Message } from "@/lib/types";
+import {useState} from "react"
 
 interface ChatWindowProps {
   userId: number | null;
@@ -8,7 +9,16 @@ interface ChatWindowProps {
   handleDeleteMessage: (id: number | null) => void;
 }
 
-export default function ChatWindow({ chatId }: ChatWindowProps) {
+export default function ChatWindow({ 
+    chatId,
+    userId,
+    messages,
+    setNewMessage,
+    handleDeleteMessage,
+ }: ChatWindowProps) {
+
+  const [inputValueMessage, setInputValueMessage] = useState("");
+
   return (
     <div
       className={`sm:flex ${chatId ? "flex" : "hidden"} flex-1 flex-col h-full max-h-screen relative`}
@@ -20,6 +30,19 @@ export default function ChatWindow({ chatId }: ChatWindowProps) {
           </h2>
         </div>
       )}
+
+      <div className="flex-1 overflow-y-auto p-4 flex flex-col gap-2 bg-gray-50">
+        {/* {messages.map((messages)=>{
+         <MessageBubble></MessageBubble>
+        })} */}
+      </div>
+
+      <div className="flex-shrink-0 p-4 border-t border-gray-200 bg-white">
+        <div>
+          <input/>
+          <button>Send</button>
+        </div>
+      </div>
     </div>
   );
 }
