4cae4e5329acd50bc50f9eef9272d3266146fa54
Ksana Ivanova <ksenia.rad.00@mail.ru>
2026-03-16 08:38:23 +0300

    feat: add update chat api

--- lib/api/chats.ts
+++ lib/api/chats.ts
@@ -1,19 +1,49 @@
 "use client";
 import { toast } from "react-toastify";
-import { GET } from "./client";
+import { GET, PATCH, DELETE } from "./client";
 import { ApiEndpoints } from "./api-endpoints";
 import { Chat } from "../types";
 
 export const getChatsAPI = async (): Promise<Chat[] | null> => {
   try {
     const response = await GET(ApiEndpoints.CHATS(""));
+   
     if (response.ok) {
       return response.data;
     }
+    
     toast.error(`Error: ${response.message}`);
+    
     return null;
   } catch (error) {
     toast(`Error: ${error}`);
+    
     return null;
   }
 };
+
+export const updateChatsAPI = async (chatId: number, chatName: {name?: string}): Promise<Chat[] | null> => {
+  try{
+    const response = await PATCH(ApiEndpoints.CHATS(String(chatId)), chatName)
+
+    if (response.ok) {
+      return response.data;
+    }
+   
+    toast.error(`Error: ${response.message}`);
+
+    return null;
+  }catch(error){
+    toast(`Error: ${error}`);
+
+    return null;
+  }
+}
+
+export const deleteChatAPI = async (chatId: number): Promise<boolean> => {
+  try{
+    return false;
+  }catch(error){
+    return false;
+  }
+}
