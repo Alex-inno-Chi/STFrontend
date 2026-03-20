bf186b940d8d74a56bf8f5bd37299ec523640931
Ksana Ivanova <ksenia.rad.00@mail.ru>
2026-03-20 09:43:00 +0300

    feat: add sendMessageAPi and delketeMessageAPI

--- lib/api/messages.ts
+++ lib/api/messages.ts
@@ -1,6 +1,6 @@
 "use client";
 import { toast } from "react-toastify";
-import { GET } from "./client";
+import { GET, POST, DELETE } from "./client";
 import { ApiEndpoints } from "./api-endpoints";
 import { Message } from "../types";
 
@@ -19,13 +19,59 @@ export const getMessagesAPI = async (
 ): Promise<Message[] | null> => {
   try {
     const response = await GET(ApiEndpoints.MESSAGES(String(chatId)));
+    
     if (response.ok) {
       return response.data;
     }
+
     toast.error(`Error: ${response.message}`);
+
     return null;
   } catch (error) {
     toast(`Error: ${error}`);
+    
     return null;
   }
 };
+
+export const sendMessageAPI = async(
+  chatId:number,
+  content: string
+): Promise<Message | null> => {
+  try {
+    const response = await POST(ApiEndpoints.MESSAGES(String(chatId)),{content});
+    
+    if (response.ok) {
+      return response.data;
+    }
+
+    toast.error(`Error: ${response.message}`);
+    
+    return null;
+  } catch (error) {
+    toast(`Error: ${error}`);
+    
+    return null;
+  }
+}
+
+export const deleteMessageAPI = async(
+  chatId:number,
+  messageId: number
+): Promise<boolean> => {
+  try {
+    const response = await DELETE(ApiEndpoints.MESSAGES(String(chatId)), String(messageId));
+    
+    if (response.ok) {
+      return response.data;
+    }
+
+    toast.error(`Error: ${response.message}`);
+    
+    return false;
+  } catch (error) {
+    toast(`Error: ${error}`);
+    
+    return false;
+  }
+}
