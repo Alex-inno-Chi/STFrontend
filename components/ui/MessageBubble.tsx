fb699f150d8218a416f037663a52f7d5216ff64a
Ksana Ivanova <ksenia.rad.00@mail.ru>
2026-03-20 10:27:10 +0300

    fix: fix date format for messages and change style for username

--- components/ui/MessageBubble.tsx
+++ components/ui/MessageBubble.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Message } from "@/lib/types"
+import { formatDate } from "@/helpers/formatDate";
 
 interface MessageBubbleProps {
     message: Message;
@@ -16,12 +17,12 @@ export default function MessageBubble ({message, isMyOwnMessage}: MessageBubbleP
         <div className={`flex flex-col max-w-[70%] ${isMyOwnMessage ? "ml-auto items-end" : "mr-auto items-start"}`}>
             <div className={`rounded-lg px-3 py-2 ${isMyOwnMessage ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-900"}`}>
             {!isMyOwnMessage && (
-                <p>{displayName}</p>
+                <p className="font-semibold">{displayName}</p>
             )}
             <p>{message.content}</p>
             {timeSend && (
-                <p>
-                    {timeSend}
+                <p className={`mt-1 text-xs ${isMyOwnMessage ? "text-blue-100" : "text-gray-500"}`}>
+                    {formatDate(timeSend)}
                 </p>
             )}
             </div>
